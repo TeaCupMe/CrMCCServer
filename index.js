8d5e7498038b5d15a3fdbaa7cad220ea1ccca2ae
Aleksey <alesha@gilenko.net>
2021-10-15 23:01:24 +0200

    Initial Commit

--- index.js
+++ index.js
@@ -1,38 +1,84 @@
 const http = require("http");
 const fs = require("fs");
 const { get } = require("https");
+
+//////////////////////////////////
+//                              //
+//    Beginning of the setup    //
+//                              //
+//////////////////////////////////
+
+const html = fs.readFileSync("./home.html")
 const host = 'localhost';
 const port = 61600;
-// const html = fs.readFileSync("./home.html")
+var Datastore = require('nedb');
+var db = new Datastore({ filename: 'database1' });
+db.loadDatabase();
+db.find({ type: "info" }, (err, data) => {
+    if (data.length == 0) {
+        db.insert({ type: "info", lastID: 0 })
+    } else {
+        
+    }
+})
+
+////////////////////////////
+//                        //
+//    End of the setup    //
+//                        //
+////////////////////////////
+
+
 /**
- * 
  * @param {http.IncomingMessage} req 
  * @param {http.ServerResponse} res 
  */
-function requestListener(req, res) {
-    console.log(getAliasOnPosition(req.url, 1))
+async function requestListener(req, res) {
     if (getAliasOnPosition(req.url, 1) != "crcommand") return;
 
     if (req.method == "POST") {
         if (getAliasOnPosition(req.url, 2) == "submit") {
-            // console.log(req.headers)
-            // console.log(req.url)
-            // console.log(req.headers)
+            let body = [];
+            req.on('data', (chunk) => {
+                body.push(chunk);
+            }).on('end', () => {
+                body = Buffer.concat(body).toString();
+                db.find({ type: "info" }, (err, data) => {
+                    db.insert({ type: "satData", data: JSON.parse(body), id: data[0].lastID+1});
+                    console.log(typeof data[0].lastID)
+                    db.update({type: "info"}, {type:"info", lastID: data[0].lastID+1})
+                })
+                
+            });
+
             res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
             res.end("Submit successful");
             return
         }
     } else if (req.method == "GET") {
         if (getAliasOnPosition(req.url, 2) == "getLatest") {
-            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-            res.end("Latest Info:");
-        } else if (getAliasOnPosition(req.url, 2) == "getAmount"){
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' }); //TODO return last 
+            db.find({ type: "info" }, (err, array) => {
+                let _lastID = array[0].lastID;
+                console.log("last id = "+_lastID)
+                db.find({type: "satData", id: _lastID}, (err, data) => {
+                    console.log(data)
+                    res.write(JSON.stringify(data[0]));
+                    res.end();
+                    
+                })
+            })
+            // console.log(b)
+            return
+        } else if (getAliasOnPosition(req.url, 2) == "getAmount") {
             res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' }); //TODO add ability to fetch several last datasets
-            res.end("Latest Info:");
+            res.end("Latest n Info:");
+            return
         }
     }
     res.writeHead(400, { 'Content-Type': 'text/html; charset=utf-8' })
-            res.end("Invalid link")
+    res.write(html)
+    res.end("Invalid link")
 };
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
